27e97e859ed06651b0154067ff8646bcbca2f696
FilipZocktan <filipzocktan@gmail.com>
2016-03-19 23:13:27 +0100

    Here you are

--- FCIShop/src/de/teamfci/fcishop/entity/Haendler.java
+++ FCIShop/src/de/teamfci/fcishop/entity/Haendler.java
@@ -6,20 +6,22 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import de.teamfci.dataprovider.dataprovider;
 import de.teamfci.fcishop.FCIShop;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.DespawnReason;
@@ -71,14 +73,16 @@ public class Haendler implements Listener {
 		}
 	}
 	//00 01 02 03 04 05 06 07 08
-	//09 10 R1 R2 R3 R4 R5 16 17
+	//09 10 11 R1 13 R2 15 16 17
 	@EventHandler
 	public void onClick(InventoryClickEvent ev){
 		Inventory clicked = ev.getClickedInventory();
 		Player p = (Player) ev.getWhoClicked();
 		if(clicked.equals(inv)){
-			if(ev.getSlot() == 3){
-				inv = Bukkit.createInventory(null, 18, "Upgradeshop Mage");
+			ev.setCancelled(true);
+			if(ev.getRawSlot() == 3){
+				p.playSound(p.getLocation(), Sound.IRONGOLEM_HIT, 100, 1);
+				inv = Bukkit.createInventory(null, 18, "Upgradeshop Magier");
 				ItemStack Mage = new ItemStack(Material.PRISMARINE_SHARD);
 				ItemMeta magemeta = Mage.getItemMeta();
 				magemeta.setDisplayName("Magier");
@@ -105,18 +109,19 @@ public class Haendler implements Listener {
 				inv.setItem(5, Splitter);
 				ItemStack Rang1 = new ItemStack(Material.STICK);
 				ItemMeta rang1meta = Rang1.getItemMeta();
-				rang1meta.setDisplayName("Rang 1");
+				rang1meta.setDisplayName("Upgrade 1: Erfahrener Zauerer");
 				Rang1.setItemMeta(rang1meta);
-				inv.setItem(11, Rang1);
+				inv.setItem(12, Rang1);
 				ItemStack Rang2 = new ItemStack(Material.STICK);
 				ItemMeta rang2meta = Rang2.getItemMeta();
-				rang2meta.setDisplayName("Rang 2");
+				rang2meta.setDisplayName("Upgrade 1: Meister der Zauererei");
 				Rang2.setItemMeta(rang2meta);
-				inv.setItem(12, Rang2);
+				inv.setItem(14, Rang2);
 				p.openInventory(inv);
-			}
-			if(ev.getSlot() == 5){
-				inv = Bukkit.createInventory(null, 18);
+			} else {
+				if (ev.getRawSlot() == 5) {
+					p.playSound(p.getLocation(), Sound.IRONGOLEM_HIT, 100, 1);
+				inv = Bukkit.createInventory(null, 18, "Splittershop");
 				ItemStack Mage = new ItemStack(Material.PRISMARINE_SHARD);
 				ItemMeta magemeta = Mage.getItemMeta();
 				magemeta.setDisplayName("Magier");
@@ -143,18 +148,57 @@ public class Haendler implements Listener {
 				inv.setItem(5, Splitter);
 				p.openInventory(inv);
 			}
-			if(ev.getSlot() == 11){
-				if(clicked.getName().equals("Upgradeshop Mage"));{
-					String rank = dataprovider.getPlayerClass(p);
-					if(rank == "Rang 1"){
-						int money = dataprovider.getMoney(p);
-						if(money >= 100){
-							
-						}
-					}
+			}
+			
+			if(ev.getSlot() == 12){
+				if(clicked.getName().equals("Upgradeshop Magier"));{
+					p.playSound(p.getLocation(), Sound.ORB_PICKUP, 100, 1);
+//					String rank = dataprovider.getPlayerClass(p);
+//					if(rank == "Zaubelehrling"){
+//						int money = dataprovider.getMoney(p);
+//						if(money >= 100){
+//						} else {
+//							p.sendMessage("Du benötigst mehr Geld.");
+//						}
+//					} else {
+//						p.sendMessage("Du benötigst einen höheren Rang oder bist bereits in diesem Rang oder höher.");
+//					}
 				}
 			}
-			ev.setCancelled(true);
+			
+			if(ev.getSlot() == 14){
+				if(clicked.getName().equals("Upgradeshop Magier"));{
+					p.playSound(p.getLocation(), Sound.ORB_PICKUP, 100, 1);
+//					String rank = dataprovider.getPlayerClass(p);
+//					if(rank == "Erfahrener Zauberer"){
+//						int money = dataprovider.getMoney(p);
+//						if(money >= 100){
+//						} else {
+//							p.sendMessage("Du benötigst mehr Geld.");
+//						}
+//					} else {
+//						p.sendMessage("Du benötigst einen höheren Rang oder bist bereits in diesem Rang oder höher.");
+//					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent ev){
+		Action ac = ev.getAction();
+		if(ac.equals(Action.RIGHT_CLICK_AIR)){
+			Player p = ev.getPlayer();
+			if(p.getItemInHand().getItemMeta().getLore().get(1).contains("Rechtsklick um zum Spawn zu gelangen"){
+				p.performCommand("spawn");
+				p.setItemInHand(new ItemStack(Material.AIR));
+			}}
+		}
+		if(ac.equals(Action.RIGHT_CLICK_BLOCK)){
+			Player p = ev.getPlayer();
+			if(p.getItemInHand().getItemMeta().getLore().get(1).contains("Rechtsklick um zum Spawn zu gelangen")){
+				p.performCommand("spawn");
+				p.setItemInHand(new ItemStack(Material.AIR));
 		}
 	}
 	

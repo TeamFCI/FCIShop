8c4cb9795b1438904a062487d6e33b7ffb2d295b
FilipZocktan <filipzocktan@gmail.com>
2016-04-02 19:05:13 +0200

    Updated Interface

--- FCIShop/src/de/teamfci/fcishop/entity/Haendler.java
+++ FCIShop/src/de/teamfci/fcishop/entity/Haendler.java
@@ -20,6 +20,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import de.teamfci.fcishop.FCIShop;
 import net.citizensnpcs.api.CitizensAPI;
@@ -85,7 +86,7 @@ public class Haendler implements Listener {
 				p.playSound(p.getLocation(), Sound.IRONGOLEM_HIT, 100, 1);
 				inv = Bukkit.createInventory(null, 18, "Upgradeshop Magier");
 				//Mageshop
-				ItemStack Mage = new ItemStack(Material.PRISMARINE_SHARD);
+				ItemStack Mage = new ItemStack(Material.SKULL, 1, (byte) 3);
 				ItemMeta magemeta = Mage.getItemMeta();
 				magemeta.setDisplayName("Magier");
 				lore.add("§aFortress§8-§aCombat§8-§aShop");
@@ -97,27 +98,29 @@ public class Haendler implements Listener {
 				magemeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 				lore.clear();
 				Mage.setItemMeta(magemeta);
+				SkullMeta mtas = (SkullMeta) Mage.getItemMeta();
+				mtas.setOwner("MHF_Question");
+				Mage.setItemMeta(mtas);
 				inv.setItem(3, Mage);
 				//Spittershop
 				ItemStack Splitter = new ItemStack(Material.QUARTZ);
-				ItemMeta splitmeta = Mage.getItemMeta();
-				splitmeta.setDisplayName("Splittershop");
+				ItemMeta splitmeta = Splitter.getItemMeta();
+				splitmeta.setDisplayName("§4§lSpli§6§ltt§4§lershop");
 				lore.add("§aFortress§8-§aCombat§8-§aShop");
 				lore.add("§9§m------------------------------");
-				lore.add("§6Shop für Splitter.");
-				lore.add("§6Hier kannst du FCI-Items kaufen");
+				lore.add("§6§lHier kannst du dir spezielle Items für Blutsplitter besorgen.");
 				splitmeta.setLore(lore);
 				lore.clear();
 				Splitter.setItemMeta(splitmeta);
 				inv.setItem(5, Splitter);
-				//Magierorden
-				ItemStack Rang3 = new ItemStack(Material.BLAZE_POWDER);
-				ItemMeta rang3meta = Rang3.getItemMeta();
-				rang3meta.setDisplayName("Upgrade 3: Magierorden");
-				Rang3.setItemMeta(rang3meta);
-				inv.setItem(13, Rang3);
-				hm.put(p, inv);
-				p.openInventory(hm.get(p));
+//				//Magierorden
+//				ItemStack Rang3 = new ItemStack(Material.BLAZE_POWDER);
+//				ItemMeta rang3meta = Rang3.getItemMeta();
+//				rang3meta.setDisplayName("Upgrade 3: Magierorden");
+//				Rang3.setItemMeta(rang3meta);
+//				inv.setItem(13, Rang3);
+//				hm.put(p, inv);
+//				p.openInventory(hm.get(p));
 			} else {
 				if (ev.getRawSlot() == 5) {
 					p.playSound(p.getLocation(), Sound.IRONGOLEM_HIT, 100, 1);
@@ -136,7 +139,7 @@ public class Haendler implements Listener {
 				inv.setItem(3, Mage);
 				//Splittershop
 				ItemStack Splitter = new ItemStack(Material.QUARTZ);
-				ItemMeta splitmeta = Mage.getItemMeta();
+				ItemMeta splitmeta = Splitter.getItemMeta();
 				splitmeta.setDisplayName("Splittershop");
 				lore.add("§aFortress§8-§aCombat§8-§aShop");
 				lore.add("§9§m------------------------------");
